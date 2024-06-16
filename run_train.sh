4244a4e07682048fa5b8cac1d7755b142d608002
Aidan Harbison <aidan_harbison@brown.edu>
2024-06-16 02:14:23 +0200

    trying with just exclusive gpus

--- run_train.sh
+++ run_train.sh
@@ -4,8 +4,6 @@
 #SBATCH -p publicgpu
 #SBATCH -N 1
 #SBATCH --exclusive
-#SBATCH --gres=gpu:4
-#SBATCH --constraint=gputc
 #SBATCH --time 00:59:00
 #SBATCH --mail-type=END
 #SBATCH --mail-user=harbison@unistra.fr
