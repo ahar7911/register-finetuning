5b4f5202c479757496bc9bf92938ee4aa79e8985
Aidan Harbison <aidan_harbison@brown.edu>
2024-06-17 14:17:10 +0200

    faster if exclusive GPU?

--- run_eval.sh
+++ run_eval.sh
@@ -3,34 +3,34 @@
 #SBATCH -A lilpa
 #SBATCH -p publicgpu
 #SBATCH -N 1
+#SBATCH --exclusive
 #SBATCH --time 00:59:00
 #SBATCH --mail-type=END
 #SBATCH --mail-user=harbison@unistra.fr
 #SBATCH --error=output/error.txt
 #SBATCH --output=output/output.txt
 
-set CUDA_LAUNCH_BLOCKING=1
 module load python/python-3.11.3
 source /home2020/home/lilpa/harbison/experiences/env/bin/activate
 
 model=mbert
 train_langs=fr
-eval_langs=("fr" "sw" "fi")
+eval_langs=("fr" "sw")
 
-# echo "evaluating $model trained on $train_langs"
-# for lang in "${eval_langs[@]}"
-# do
-#     echo "evaluating on $lang"
-#     time python evaluate.py --model $model --train_langs $train_langs --eval_lang $lang
-#     echo "$lang complete"
-# done
-# echo "all evaluations complete"
-
-echo "evaluating untrained $model"
+echo "evaluating $model trained on $train_langs"
 for lang in "${eval_langs[@]}"
 do
     echo "evaluating on $lang"
-    time python evaluate.py --model $model --eval_lang $lang
+    time python evaluate.py --model $model --train_langs $train_langs --eval_lang $lang
     echo "$lang complete"
 done
 echo "all evaluations complete"
+
+# echo "evaluating untrained $model"
+# for lang in "${eval_langs[@]}"
+# do
+#     echo "evaluating on $lang"
+#     time python evaluate.py --model $model --eval_lang $lang
+#     echo "$lang complete"
+# done
+# echo "all evaluations complete"
