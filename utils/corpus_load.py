2d482cd6c19408140c15dcd8ff078511088edc76
Aidan Harbison <aidan_harbison@brown.edu>
2024-06-17 15:31:51 +0200

    texts are tokenized before becoming a dataset instead of while being accessed

--- utils/corpus_load.py
+++ utils/corpus_load.py
@@ -8,30 +8,30 @@ REGISTERS = ['IN', 'IP/OP', 'RN', 'JN', 'HI', 'LY', 'NID', 'AID']
 REG2ID = {reg : REGISTERS.index(reg) for reg in REGISTERS}
 
 class RegisterDataset(Dataset):
-    def __init__(self, texts, registers, tokenizer):
-        self.texts = texts
+    def __init__(self, encoded_texts : list[dict[str, torch.Tensor]], registers : list[int]):
+        self.encoded_texts = encoded_texts
         self.registers = registers
-        self.tokenizer = tokenizer
 
     def __len__(self):
-        return len(self.texts)
+        return len(self.encoded_texts)
 
     def __getitem__(self, index : int):
-        text = str(self.texts[index])
+        encoded_text = self.encoded_texts[index]
         register = self.registers[index]
-        encoded_text = self.tokenizer(text, return_tensors = "pt", padding = "max_length", truncation=True)
         return {**encoded_text, 'labels' : torch.tensor(register, dtype=torch.long)}
 
 
 def load_data(filepath : str, model_checkpoint : str, batch_size : int=16) -> DataLoader:
     dataset = pd.read_csv(filepath, sep='\t')
-    
-    X_texts = dataset.iloc[:,1].tolist()
-    y_regs = dataset.iloc[:,0].tolist()
-    y_regs = [REG2ID[reg] for reg in y_regs]
-
     tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
-    dataset = RegisterDataset(texts=X_texts, registers=y_regs, tokenizer=tokenizer)
+    
+    texts = dataset.iloc[:,1].tolist()
+    encoded_texts = tokenizer(texts, return_tensors="pt", padding="max_length", truncation=True)
+    regs = dataset.iloc[:,0].tolist()
+    regs = [REG2ID[reg] for reg in regs]
+    
+    
+    dataset = RegisterDataset(encoded_texts, regs)
     dataloader = DataLoader(dataset, batch_size=batch_size)
 
     return dataloader
