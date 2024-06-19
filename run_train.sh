46f08333780eb135b4b5ad43ef77dcac05babb41
Aidan Harbison <aidan_harbison@brown.edu>
2024-06-20 00:22:42 +0200

    these for loops are not running...

--- run_train.sh
+++ run_train.sh
@@ -14,8 +14,10 @@ source /home2020/home/lilpa/harbison/experiences/env/bin/activate
 models=("mbert" "xlm-r" "glot500")
 lang=("en" "fi" "fr" "sw")
 
-for model in "${models[@]}"; do
-    for lang in "${langs[@]}"; do
+for model in "${models[@]}"
+do
+    for lang in "${langs[@]}"
+    do
         echo "finetuning $model on $lang"
         time python finetune.py --model $model --train_langs $lang
         echo "finetuning $model on $lang complete"
