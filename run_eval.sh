34baf85ca71940e9ffab6f78f619bafc3f09f73b
Aidan Harbison <aidan_harbison@brown.edu>
2024-06-21 12:15:56 +0200

    eval multiple models trained on multiple languages on multiple languages

--- run_eval.sh
+++ run_eval.sh
@@ -11,24 +11,18 @@
 module load python/python-3.11.3
 source /home2020/home/lilpa/harbison/experiences/env/bin/activate
 
-model=mbert
-train_langs=fr
-eval_langs=("fr" "sw")
+models=("mbert" "xlm-r" "glot500")
+train_langs=("fi" "fr" "sv")
+eval_langs=("en" "fi" "fr" "sw")
 
-echo "evaluating $model trained on $train_langs"
-for lang in "${eval_langs[@]}"
-do
-    echo "evaluating on $lang"
-    time python evaluate.py --model $model --train_langs $train_langs --eval_lang $lang
-    echo "$lang complete"
+for model in "${models[@]}"; do
+    for train_lang in "${train_langs[@]}"; do
+        echo "evaluating $model trained on $train_langs"
+        for eval_lang in "${eval_langs[@]}"; do
+            echo "evaluating on $eval_lang"
+            time python evaluate.py --model $model --train_langs $train_lang --eval_lang $eval_lang
+            echo "$eval_lang complete"
+        done
+    done
 done
 echo "all evaluations complete"
-
-# echo "evaluating untrained $model"
-# for lang in "${eval_langs[@]}"
-# do
-#     echo "evaluating on $lang"
-#     time python evaluate.py --model $model --eval_lang $lang
-#     echo "$lang complete"
-# done
-# echo "all evaluations complete"
