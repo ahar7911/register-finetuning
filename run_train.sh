79a53302f0f7638f07910a3521e7ac6f431a62bd
Aidan Harbison <aidan_harbison@brown.edu>
2024-06-21 11:26:30 +0200

    train all models except english ?

--- run_train.sh
+++ run_train.sh
@@ -11,10 +11,8 @@
 module load python/python-3.11.3
 source /home2020/home/lilpa/harbison/experiences/env/bin/activate
 
-#models=("mbert" "xlm-r" "glot500")
-#langs=("en" "fi" "fr" "sv")
-models=("mbert")
-langs=("en")
+models=("mbert" "xlm-r" "glot500")
+langs=("fi" "fr" "sv")
 
 for model in "${models[@]}"; do
     for lang in "${langs[@]}"; do
