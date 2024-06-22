c8e45eef68373984ed13ec637cedf7725b08e74f
Aidan Harbison <aidan_harbison@brown.edu>
2024-06-22 07:15:26 +0200

    finetune all models on tr and id

--- run_train.sh
+++ run_train.sh
@@ -12,7 +12,7 @@ module load python/python-3.11.3
 source /home2020/home/lilpa/harbison/experiences/env/bin/activate
 
 models=("mbert" "xlm-r" "glot500")
-langs=("en")
+langs=("tr" "id")
 
 for model in "${models[@]}"; do
     for lang in "${langs[@]}"; do
