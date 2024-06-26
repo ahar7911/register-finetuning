f07e94e8828d3a26481f04b6c023c1f696e4d8b6
Aidan Harbison <aidan_harbison@brown.edu>
2024-06-26 05:18:56 +0200

    reduce to one hour, only one model training

--- run_train.sh
+++ run_train.sh
@@ -4,7 +4,7 @@
 #SBATCH -N 1
 #SBATCH --gres=gpu:2
 #SBATCH --exclusive
-#SBATCH --time 01:59:00
+#SBATCH --time 00:59:00
 
 module load python/python-3.11.3
 source /home2020/home/lilpa/harbison/experiences/env/bin/activate
