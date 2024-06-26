317a29542974a6d546fc714a9a2d2f90529c18f7
Aidan Harbison <aidan_harbison@brown.edu>
2024-06-26 03:30:58 +0200

    use srun

--- run_eval.sh
+++ run_eval.sh
@@ -21,7 +21,7 @@ for model in "${models[@]}"; do
         for eval_lang in "${eval_langs[@]}"; do
             echo "evaluating on $eval_lang"
             mkdir -p "output/$model/$train_lang/eval"
-            time python evaluate.py --model $model --train_langs $train_lang --eval_lang $eval_lang
+            srun python evaluate.py --model $model --train_langs $train_lang --eval_lang $eval_lang
             echo "$eval_lang complete"
         done
     done
