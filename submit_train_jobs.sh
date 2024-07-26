e0cab830f68b73ffa63c137ae95fdfc39fbc1914
Aidan Harbison <aidan_harbison@brown.edu>
2024-07-26 13:08:59 +0200

    moved making output folder back to submit_train_jobs

--- submit_train_jobs.sh
+++ submit_train_jobs.sh
@@ -3,11 +3,10 @@
 models=("mbert")
 langs=("fr")
 
-mkdir -p output
-
 for model in "${models[@]}"; do
     for lang in "${langs[@]}"; do
         output_dir="output/$model-$lang"
+        mkdir -p $output_dir
         echo "submitting finetuning job for $model on $lang"
         sbatch --job-name="finetune_${model}_${lang}" --output="$output_dir/train.txt" run_train.sh $model $lang
     done
