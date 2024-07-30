cf1f5bf3e2c881415a11b3e2fd1734ecee86eab1
Aidan Harbison <aidan_harbison@brown.edu>
2024-07-30 23:10:34 +0200

    train glot500 on all except id

--- submit_train_jobs.sh
+++ submit_train_jobs.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-models=("mbert" "xlmr" "glot500")
-langs=("en" "fi" "fr" "sv" "id" "tr")
+models=("glot500")
+langs=("en" "fi" "fr" "sv" "tr")
 
 for model in "${models[@]}"; do
     for lang in "${langs[@]}"; do
