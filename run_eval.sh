c41cc9fcdc02e9f5d0f105d39ad4202a4171d04e
Aidan Harbison <aidan_harbison@brown.edu>
2024-07-31 10:27:53 +0200

    separate eval jobs by finetuned models

--- run_eval.sh
+++ run_eval.sh
@@ -1,27 +1,25 @@
 #! /bin/bash
-#SBATCH --job-name=evaluate-register
 #SBATCH -A lilpa
 #SBATCH -p publicgpu
 #SBATCH -N 1
 #SBATCH --exclusive
-#SBATCH --time 01:59:00
-#SBATCH --output=output/eval.txt
+#SBATCH --time 00:29:00
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=harbison@unistra.fr
+
+set -e # if python script fails, bash script fails
 
 source load_env.sh
 
-models=("mbert" "xlmr" "glot500")
-train_langs=("en" "fi" "fr" "sv" "id" "tr")
-eval_langs=("ar" "ca" "es" "fa" "hi" "jp" "no" "pt" "ur" "zh")
+model=$1
+train_lang=$2
+eval_langs=("en" "fi" "fr" "sv" "id" "tr" "ar" "ca" "es" "de" "fa" "hi" "jp" "no" "pt" "ur" "zh")
 
-for model in "${models[@]}"; do
-    for train_lang in "${train_langs[@]}"; do
-        echo "evaluating $model trained on $train_lang"
-        for eval_lang in "${eval_langs[@]}"; do
-            echo "evaluating on $eval_lang"
-            srun python evaluate.py --model $model --train_langs $train_lang --eval_lang $eval_lang
-            echo "$eval_lang complete"
-            echo
-        done
-    done
+echo "evaluating $model trained on $train_lang"
+for eval_lang in "${eval_langs[@]}"; do
+    echo "evaluating on $eval_lang"
+    srun python evaluate.py --model $model --train_langs $train_lang --eval_lang $eval_lang
+    echo "$eval_lang complete"
+    echo
 done
 echo "all evaluations complete"
---
+++ submit_eval_jobs.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+models=("mbert" "xlmr" "glot500")
+langs=("en" "fi" "fr" "id" "sv" "tr")
+
+for model in "${models[@]}"; do
+    for lang in "${langs[@]}"; do
+        output_dir="output/$model-$lang"
+        mkdir -p $output_dir
+        echo "submitting evaluation job for $model on $lang"
+        sbatch --job-name="evaluate-${model}-${lang}" --output="$output_dir/eval.txt" run_eval.sh $model $lang
+    done
+done
+echo "all jobs submitted"
