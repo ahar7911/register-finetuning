1b3e545be55ebdb4d27ac4d0280ac307e41dbea0
Aidan Harbison <aidan_harbison@brown.edu>
2024-08-13 11:46:07 -0400

    remove debugging statement

--- utils/cfm.py
+++ utils/cfm.py
@@ -16,7 +16,6 @@ def plot_summary(ax : matplotlib.axes.Axes, train_lang : str) -> None:
     train_lang_tsvs = [Path(f"train/{train_lang}.tsv") for train_lang in train_lang.split("-")]
     registers = []
     for train_lang_tsv in train_lang_tsvs:
-        print(train_lang_tsv)
         _, train_lang_regs = get_texts_regs(train_lang_tsv)
         registers.extend(train_lang_regs)
     register_counts = Counter(registers)
